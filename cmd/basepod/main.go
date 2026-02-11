0e5cd8060071e88c5377f509ea7132da4d9b7180
Flakerim Ismani <flakerimi@gmail.com>
2026-02-11 18:20:04 +0100

    Release v2.0.6

--- cmd/basepod/main.go
+++ cmd/basepod/main.go
@@ -28,7 +28,7 @@ import (
 )
 
 var (
-	version = "2.0.5"
+	version = "2.0.6"
 
 	// Release URL for updates (uses GitHub releases API)
 	releaseBaseURL = "https://github.com/base-go/basepod/releases/latest/download"
--- cmd/bp/main.go
+++ cmd/bp/main.go
@@ -25,7 +25,7 @@ import (
 )
 
 var (
-	version = "2.0.5"
+	version = "2.0.6"
 )
 
 // ServerConfig holds configuration for a single server
