170e541e4a84296c8ab495c0a950802c6656b83e
Flakerim Ismani <flakerimi@gmail.com>
2026-02-12 01:26:46 +0100

    Release v2.0.13

--- cmd/basepod/main.go
+++ cmd/basepod/main.go
@@ -28,7 +28,7 @@ import (
 )
 
 var (
-	version = "2.0.12"
+	version = "2.0.13"
 
 	// Release URL for updates (uses GitHub releases API)
 	releaseBaseURL = "https://github.com/base-go/basepod/releases/latest/download"
